33fea56d6cddc884b19268d096359898af4f12ce
Nawaz <nawazhaider60@gmail.com>
2023-06-22 21:58:39 +0600

    added CSS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { Component } from 'react';
 import './App.css';
+import CardList from './card-list/card-list.component.jsx';
+import SearchBox from './search-box/search-box.components';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
 
   componentDidMount() {
-    fetch('http://localhost:8080/users')
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => {
         this.setState(
@@ -24,16 +27,30 @@ class App extends Component {
       });
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    const { searchField, monsters } = this.state;
+    const { onSearchChange } = this;
+
+    const newMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
+        <h1 className="app-header">Monster Rolodex</h1>
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          className="search-box"
+          placeholder="search monsters"
+        />
+        <CardList monsters={newMonsters} />
       </div>
     );
   }
---
+++ src/card-list/card-list.component.jsx
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import Card from './card/card.component.jsx';
+
+class CardList extends Component {
+  render() {
+    const { monsters } = this.props;
+
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => {
+          return <Card monster={monster} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default CardList;
